2164679b3cba541402094d39236d59d6e5369e3a
Syed Muhammad Danish <danishshah2203651@gmail.com>
2024-07-15 17:46:20 +0500

    navbar added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Navbar from "@/components/Navbar";
+import 'boxicons/css/boxicons.min.css';
 
 export default function Home() {
   return (
     <>
-    
+      <Navbar/>
     </>
   );
 }
