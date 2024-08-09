6adc3add4ef68bffef2fd22813b0eb348601550b
Syed Muhammad Danish <danishshah2203651@gmail.com>
2024-08-09 19:41:50 +0500

    Added Contact Section and Footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,54 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect, useState } from "react";
 import Navbar from "@/components/Navbar";
 import HomeSection from "@/components/HomeSection";
 import AboutSection from "@/components/AboutSection";
 import ServicesSection from "@/components/ServicesSection";
 import PortfolioSection from "@/components/PortfolioSection";
-import 'boxicons/css/boxicons.min.css';
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+import "typeface-poppins";
+import "boxicons/css/boxicons.min.css";
 
 export default function Home() {
+  const [activeSection, setActiveSection] = useState("");
+
+  const handleScroll = () => {
+    const sections = document.querySelectorAll("section");
+    const scrollPosition = window.scrollY + 150; 
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+      const sectionId = section.getAttribute("id");
+
+      if (
+        scrollPosition >= sectionTop &&
+        scrollPosition < sectionTop + sectionHeight
+      ) {
+        setActiveSection(sectionId);
+      }
+    });
+  };
+
+  useEffect(() => {
+    handleScroll();  
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <Navbar />
+      <Navbar activeSection={activeSection} />
       <HomeSection />
       <AboutSection />
       <ServicesSection />
       <PortfolioSection />
+      <ContactSection />
+      <Footer />
     </>
   );
 }
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,42 +1,57 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import './index.css';
+"use client";
+import React, { useEffect, useState } from "react";
+import "./index.css";
 
-export default function Navbar() {
+export default function Navbar({ activeSection }) {
   const [isSticky, setIsSticky] = useState(false);
 
   const handleScroll = () => {
-    if (window.scrollY > 50) {
-      setIsSticky(true);
-    } else {
-      setIsSticky(false);
-    }
+    setIsSticky(window.scrollY > 50);
   };
 
   useEffect(() => {
     handleScroll();
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
-    <header className={`header ${isSticky && 'sticky'}`}>
-      <a href='/' className='logo'>Portfolio.</a>
+    <header className={`header ${isSticky ? "sticky" : ""}`}>
+      <a href="/" className="logo">
+        Portfolio.
+      </a>
 
-      <nav className='navbar'>
-        <a href='#home' className='active'>Home</a>
-        <a href='#about'>About</a>
-        <a href='#services'>Services</a>
-        <a href='#portfolio'>Portfolio</a>
-        <a href='#contact'>Contact</a>
+      <nav className="navbar">
+        <a href="#home" className={activeSection === "home" ? "active" : ""}>
+          Home
+        </a>
+        <a href="#about" className={activeSection === "about" ? "active" : ""}>
+          About
+        </a>
+        <a
+          href="#services"
+          className={activeSection === "services" ? "active" : ""}
+        >
+          Services
+        </a>
+        <a
+          href="#portfolio"
+          className={activeSection === "portfolio" ? "active" : ""}
+        >
+          Portfolio
+        </a>
+        <a
+          href="#contact"
+          className={activeSection === "contact" ? "active" : ""}
+        >
+          Contact
+        </a>
       </nav>
 
-      <div className='bx bx-moon' id='darkMode-icon'>
-
-      </div>
+      <div className="bx bx-moon" id="darkMode-icon"></div>
     </header>
-  )
+  );
 }
